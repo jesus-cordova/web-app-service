09373a0e8082da9360b9630eba42a23a96178204
Jesus Cordova <jjcordovajj@outlook.com>
2023-10-09 14:42:25 -0600

    first commit

---
+++ src/main/java/com/webapp/webappservice/WebappserviceApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.webappservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webapp/webappservice/WebappserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.webappservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
